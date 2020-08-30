15ae55c4fea79ab8df0af3699a941d692f5650a8
gauravraj0510 <62883388+gauravraj0510@users.noreply.github.com>
2020-08-30 14:51:14 +0530

    delete from mid

---
+++ Singly Linked List/delete_mid.c
@@ -0,0 +1,80 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct node *create(int);
+void display(struct node *);
+void deleteMid(struct node *);
+struct node
+{
+    int data;
+    struct node *next;
+};
+
+int main() {
+    printf("Enter the number of nodes:\n");
+    int n;
+    scanf("%d",&n);
+    struct node *HEAD;
+    HEAD = create(n);
+    display(HEAD);
+    deleteMid(HEAD);
+    display(HEAD);
+    return 0;
+}
+
+struct node *create(int n) {
+    struct node *HEAD, *P;
+    int i;
+    HEAD = (struct node *)malloc(sizeof(struct node));
+    if(HEAD == NULL){
+        printf("Memory not available!\n");
+    }
+    else{
+        printf("Enter data for node 1:");
+        scanf("%d",&(HEAD->data));
+        HEAD->next = NULL;
+        P = HEAD;
+
+        for(i=2; i <= n; i++){
+            P->next = (struct node *)malloc(sizeof(struct node));
+            if(P->next == NULL){
+                printf("Memory not available!\n");
+                break;
+            }
+            else{
+                P = P->next;
+                printf("Enter data for node %d:", i);
+                scanf("%d",&(P->data));    
+            }
+            P->next = NULL;
+        }
+    }
+    return HEAD;
+}
+
+void deleteMid(struct node *HEAD){
+    struct node *PREV, *TARGET;
+    int key;
+    printf("\nEnter the data of node to be deleted:");
+    scanf("%d",&key);
+    PREV = HEAD;
+    while (key != PREV->next->data && PREV->next != NULL)
+    {
+        PREV = PREV->next;
+    }
+    if(PREV->next != NULL){
+        TARGET = PREV->next;
+        PREV->next = PREV->next->next;
+    }
+    free(TARGET);
+}
+
+void display(struct node *HEAD){
+    struct node *P;
+    P = HEAD;
+    printf("Data stored in linked list are:\n");
+    while (P != NULL)
+    {
+        printf("\t %d",(P->data));
+        P = P->next;
+    }
+}
